5a02265aa27029e92fddb3818da61caf9f7d01b7
Elijah Triumph Angengeun <eliyang256@outlook.com>
2024-07-22 23:28:21 +0300

    remove fade

--- src/routes/get_in_touch/+page.svelte
+++ src/routes/get_in_touch/+page.svelte
@@ -9,19 +9,19 @@
 
 <section class="pb-24 pt-4">
     <div class="grid gap-y-44 sm:grid-cols-2 text-2xl md:text-3xl">
-        <div class="grid obs fade-in">
+        <div class="grid">
             <p class="">
                 The fastest way to get in touch is telephone, we are on the other side of this line for any inquiries, don't hesitate to hit us up at <br><a class="text-primary font-bold" href="tel:+256754441930">+256 754 441 930</a>
             </p>
         </div>
         <div class="hidden sm:block"></div>
         <div class="hidden sm:block"></div>
-        <div class="grid obs fade-in">
+        <div class="grid">
             <p class="">
                 Prefer text?<br>We've also got an active email that is regularly checked, feel free to send an email  <br><a class="text-primary font-bold" href="mailto:kamysdelightslimited@gmail.com">kamysdelightslimited <span>@gmail.com</span></a>
             </p>
         </div>
-        <div class="grid obs fade-in">
+        <div class="grid">
             <svg class="fill-secondary h-16 mx-auto hidden" viewBox="0 0 60 60">
                 <path
                     d="M30 3.75C24.5319 3.75645 19.2896 5.93151 15.4231 9.79805C11.5565 13.6646 9.38146 18.9069 9.37501 24.375C9.36999 28.8433 10.8295 33.1901 13.53 36.75C13.53 36.75 14.0925 37.4906 14.1844 37.5975L30 56.25L45.8231 37.5881C45.9056 37.4888 46.47 36.75 46.47 36.75L46.4719 36.7444C49.1706 33.1858 50.6294 28.8412 50.625 24.375C50.6186 18.9069 48.4435 13.6646 44.577 9.79805C40.7104 5.93151 35.4681 3.75645 30 3.75ZM30 31.875C28.5167 31.875 27.0666 31.4351 25.8332 30.611C24.5999 29.7869 23.6386 28.6156 23.0709 27.2451C22.5033 25.8747 22.3547 24.3667 22.6441 22.9118C22.9335 21.457 23.6478 20.1206 24.6967 19.0717C25.7456 18.0228 27.082 17.3085 28.5368 17.0191C29.9917 16.7297 31.4997 16.8782 32.8701 17.4459C34.2406 18.0136 35.4119 18.9749 36.236 20.2082C37.0601 21.4416 37.5 22.8916 37.5 24.375C37.4975 26.3634 36.7066 28.2696 35.3006 29.6756C33.8946 31.0815 31.9884 31.8725 30 31.875Z"
